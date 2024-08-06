13ebbd2b08c5992efc0340b26e23bc1f6d5bd369
fulcanelly <dallasfulcanelly@gmail.com>
2024-08-06 17:17:15 +0300

    fut: add file delition to model

--- models/file.js
+++ models/file.js
@@ -1,7 +1,13 @@
 'use strict';
+
 const {
   Model
 } = require('sequelize');
+
+const fs = require('fs');
+const util = require('util');
+const unlink = util.promisify(fs.unlink);
+
 module.exports = (sequelize, DataTypes) => {
   class File extends Model {
     /**
@@ -9,6 +15,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+
+    deleteFile() {
+      return unlink(this.path)
+    }
+
     static associate(models) {
       File.belongsTo(models.User, {
         foreignKey: 'userId'
