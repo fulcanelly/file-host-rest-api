0f837091d682e8182a8aa1eda23c70c9926c10c6
fulcanelly <dallasfulcanelly@gmail.com>
2024-08-07 03:11:37 +0300

    fut: add token blacklisting method to session model, hooks

--- models/session.js
+++ models/session.js
@@ -1,7 +1,10 @@
 'use strict';
 const {
   Model
-} = require('sequelize');
+} = require('sequelize')
+const { redisClient } = require('../lib/redis')
+const { config } = require('../config')
+
 module.exports = (sequelize, DataTypes) => {
   class Session extends Model {
     /**
@@ -9,6 +12,21 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    async blacklistAll() {
+      await Promise.all([
+        this.blacklistAccessToken(),
+        this.blacklistRefreshToken()
+      ])
+    }
+
+    async blacklistAccessToken() {
+      await redisClient.set('blacklist:' + this.accessPart, this.accessHash, { EX: config.expireBlacklistRedis })
+    }
+
+    async blacklistRefreshToken() {
+      await redisClient.set('blacklist:' + this.refreshPart, this.refreshHash, { EX: config.expireBlacklistRedis })
+    }
+
     static associate(models) {
       Session.belongsTo(models.User, {
         foreignKey: 'userId'
@@ -26,6 +44,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Session',
+    hooks: {
+      beforeDestroy: async (session, opts) => {
+        await session.blacklistAll()
+      }
+    }
   });
   return Session;
 };
